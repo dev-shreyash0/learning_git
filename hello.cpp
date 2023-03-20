070ca1d743fbf69d0e2b60b026418d329794f682
SHREYASH <shreyash@SHREYASHs-MacBook-Air.local>
2023-03-20 11:30:35 +0530

    function added

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
 
+void print() {
+	cout << "Fine!" << endl;
+}
+
 int main(void) {
 	cout << "Hello, World" << endl;
 	cout << "How's it going ?" << endl;
+	print();
 	return 0;
 }
